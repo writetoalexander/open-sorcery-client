d1ea9a61cf1c42be3da5ba224c511e4656e20ec7
AlexBoerschinger <writetoalexander@gmail.com>
2019-01-31 14:55:19 -0500

    2nd readme update

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -5,10 +5,6 @@ import store from '../../store/store';
 const Search = (props) => {
   console.log('state inside search', store.getState())
   return (
-    // <div className="repo-search">
-    //   <input onChange={props.onChange}></input>
-    //   <button id="search-button" onClick={props.searchGithubRepos}>Search Repos</button>
-    // </div>
     <form >
       <div>
         <div>
@@ -22,9 +18,8 @@ const Search = (props) => {
       </div>
       <button  type="button" onClick={
         () => {
-        //console.log('store.getState.form yields ', store.getState().form) ; 
+        
         let query = store.getState().form.simple.values.firstName
-        //console.log('query is ', query);
 
         props.handleActionSearch(query);
       }
